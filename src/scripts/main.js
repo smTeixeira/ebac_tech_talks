ee578998bf1a07b345a31d7260d67df27990e24a
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-10-12 12:49:56 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Oct 10, 2023 19:00:00");
+const dataDoEvento = new Date("Oct 30, 2023 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function () {
